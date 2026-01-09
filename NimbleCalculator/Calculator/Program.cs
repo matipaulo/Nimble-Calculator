b46b1971b60489c00bee08faf93e5bbc66979c1f
matipaulo <matipaulo@hotmail.com>
2026-01-09 15:54:32 -0300

    Changes as per code review

--- NimbleCalculator/Calculator/Program.cs
+++ NimbleCalculator/Calculator/Program.cs
@@ -10,10 +10,16 @@ while (true)
 
     if (input is null || string.IsNullOrWhiteSpace(input))
         continue;
-    
-    var numbers = InputParser.ParseInput(input);
-    
-    var operation = new SumOperation();
-    Console.WriteLine(operation.Execute(numbers[0], numbers[1]));
-}
 
+    try
+    {
+        var numbers = InputParser.ParseInput(input);
+
+        var operation = new SumOperation();
+        Console.WriteLine(operation.Execute(numbers[0], numbers[1]));
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"An unexpected error occurred: {ex.Message}");
+    }
+}
