5e4489beec742cd5fc8a37e2f5b6dc6c2324c613
matipaulo <matipaulo@hotmail.com>
2026-01-09 17:37:43 -0300

    Support multiple delimiters

---
+++ NimbleCalculator/Calculator.Tests/Parsers/DelimiterStrategyTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Calculator.Tests.Parsers;
+
+using Calculator.Parsers;
+
+public class DefaultDelimiterStrategyTests
+{
+    [Fact]
+    public void CanHandle_ReturnsTrue_WhenInputDoesNotDeclareCustomDelimiter()
+    {
+        var strategy = new DefaultDelimiterStrategy();
+        Assert.True(strategy.CanHandle("1,2,3"));
+    }
+
+    [Fact]
+    public void CanHandle_ReturnsFalse_WhenInputDeclaresCustomDelimiter()
+    {
+        var strategy = new DefaultDelimiterStrategy();
+        Assert.False(strategy.CanHandle("//;\n1;2"));
+    }
+
+    [Fact]
+    public void Extract_ReturnsInputAndDefaultDelimiters()
+    {
+        var strategy = new DefaultDelimiterStrategy();
+        var (inputToParse, delimiters) = strategy.Extract("1,2\n3");
+
+        Assert.Equal("1,2\n3", inputToParse);
+        Assert.Equal([",", "\n"], delimiters);
+    }
+}
